dfbeee44245e2c5dcfcec9d3a26b806b470e0d70
username <youremail@domain.com>
2022-07-22 18:17:06 -0400

    current project

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+
+// start with markup for the basic layout of the UI
