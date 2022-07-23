57c213fcd7221d501e0550a6827806ad3bcdabb1
username <youremail@domain.com>
2022-07-22 20:07:55 -0400

    currently

--- js/app.js
+++ js/app.js
@@ -5,3 +5,9 @@
 /*----- functions -----*/
 
 // start with markup for the basic layout of the UI
+// Listing constants
+
+const suits = ['club', 'diamond', 'heart', 'spade'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+classCard // suit, value
