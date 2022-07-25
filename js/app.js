5dc0001e22238b6c709d9cf0c5c811904d9a4ac4
username <youremail@domain.com>
2022-07-25 14:56:38 -0400

    current

--- js/app.js
+++ js/app.js
@@ -6,77 +6,60 @@
 
 // start with markup for the basic layout of the UI
 // Listing constants
-
-//const suits = ['club', 'diamond', 'heart', 'spade'];
-//const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-
 //classCard // suit, value
 
 /*----- constants -----*/
-const suits = ['s', 'c', 'd', 'h'];
-const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const suits = ['c', 'd', 'h', 's'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const game = {
+    players: [
+        {name: 'player', wonDeck:[]},
+        {name: 'computer', wonDeck: []}, 
+    ], 
+    winner: null,
+    deck: [],
+    
+};
+const remainingText = document.getElementById('remaining'); //where can I add this into html
+const deal = document.getElementById('draw');
 
 // Build a 'master' deck of 'card' objects used to create shuffled decks
 const masterDeck = buildMasterDeck();
 renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
 
 /*----- app's state (variables) -----*/
+let scores;
+let winner;
+let choices;
 let shuffledDeck;
 
+
 /*----- cached element references -----*/
+const playerScoreEl= document.querySelector('#playerScore');
+const comScoreEl= document.querySelector('#comScore');
+const choicesEl= {
+    player: {
+        borderEl: document.querySelector('#playerScore'),
+        imgEl: document.querySelector('#playerScore > img')
+    },
+   computer: {
+        borderEl:document.querySelector('#comScore'),
+        imgEl: document.querySelector('#comScore > img')
+    }
+}
 const shuffledContainer = document.getElementById('shuffled-deck-container');
 
 /*----- event listeners -----*/
 document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
 
+
 /*----- functions -----*/
-function getNewShuffledDeck() {
-  // Create a copy of the masterDeck (leave masterDeck untouched!)
-  const tempDeck = [...masterDeck];
-  const newShuffledDeck = [];
-  while (tempDeck.length) {
-    // Get a random index for a card still in the tempDeck
+function getNewShuffledDeck(){
+const tempDeck=[...masterDeck];
+const newShuffledDeck= [];
+while (tempDeck.length) {
     const rndIdx = Math.floor(Math.random() * tempDeck.length);
-    // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
-    newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-  }
-  return newShuffledDeck;
+}
+return newShuffledDeck;
 }
 
-function renderNewShuffledDeck() {
-  // Create a copy of the masterDeck (leave masterDeck untouched!)
-  shuffledDeck = getNewShuffledDeck();
-  renderDeckInContainer(shuffledDeck, shuffledContainer);
-}
-
-function renderDeckInContainer(deck, container) {
-  container.innerHTML = '';
-  // Let's build the cards as a string of HTML
-  let cardsHtml = '';
-  deck.forEach(function(card) {
-    cardsHtml += `<div class="card ${card.face}"></div>`;
-  });
-  // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
-  // const cardsHtml = deck.reduce(function(html, card) {
-  //   return html + `<div class="card ${card.face}"></div>`;
-  // }, '');
-  container.innerHTML = cardsHtml;
-}
-
-function buildMasterDeck() {
-  const deck = [];
-  // Use nested forEach to generate card objects
-  suits.forEach(function(suit) {
-    ranks.forEach(function(rank) {
-      deck.push({
-        // The 'face' property maps to the library's CSS classes for cards
-        face: `${suit}${rank}`,
-        // Setting the 'value' property for game of blackjack, not war
-        value: Number(rank) || (rank === 'A' ? 11 : 10)
-      });
-    });
-  });
-  return deck;
-}
-
-renderNewShuffledDeck();
