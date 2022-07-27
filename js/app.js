b4c03475742e3b7bf5ef49169c513739c5ca4a07
username <youremail@domain.com>
2022-07-27 19:17:28 -0400

    current commit

--- js/app.js
+++ js/app.js
@@ -6,77 +6,131 @@
 
 // start with markup for the basic layout of the UI
 //classCard // suit, value
-
-let playerScore = 0
-let comScore = 0
-const cardsContainer = document.getElementById('cards')
-const remainingText= document.getElementById('remaining')
-const playerScoreEl = document.getElementById('playerScore')
-const comScoreEl= document.getElementById('comScore')
-let firstRun= true;
-let cards = [];
+/*----- constants -----*/
+const suits = ['d', 'c', 'h', 's'];
+const cardFace = ['2','3','4','5','6','7','8','9','10','J','K','Q','A'];
+let deck = [];
+let players = [[],[]];
+let firstRun = true;
+let gameOver = false;
 let drawButton = document.querySelector('#btnDraw');
+let p1 = document.querySelector('#player .hand');
+let p2 = document.querySelector('#player2 .hand');
+let s1 = document.querySelector('#player .score');
+let s2 = document.querySelector('#player2 .score');
 
-//event listeners 
+
+// event listner
 drawButton.addEventListener('click', draw);
 
 //functions
-
-//button function
 function draw(){
-  console.log('works');  
-}; 
-
-
-//create a new card object with corresponding value and suit for deck
-
-class Deck{
-  constructor()// this helps to provide custom initialization that must be done before any other methods can be called on an instantiated object. This is a base class, therefore it is empty 
-  {
-    this.deck = [];
-    this.reset();
-    this.shuffle();
+  if(firstRun){
+    firstRun = false;
+    buildCards();
+    shuffleArray(deck);
+    dealCards(deck);
   }
 
-  //create a reset function that will give players a fresh, random and shuffled set of cards
-  reset(){
-    this.deck= [];
-    const suits = ['c', 'd', 'h', 's'];
-    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  attack();
+  //console.log('works');
+}
 
-    for (let suit in suits) {
-      for (let value in values) {
-        this.deck.push(`${values[value]} of ${suits[suit]}`);
-      }
+function attack(){
+  if(!gameOver){
+    let card1 = players[0].shift();
+    let card2 = players[1].shift();
+    let pot = [card1,card2];
+    //update html
+    p1.innerHTML = showCard(card1,0);
+    p2.innerHTML = showCard(card2,0);
+    //check winners
+    checkWinner(card1,card2,pot);
+    //update scores
+    s1.innerHTML = players[0].length;
+    s2.innerHTML = players[1].length;
+  }
+}
+
+function checkWinner(card1,card2,pot){
+  console.log(card1,card2);
+  if(card1.cardValue > card2.cardValue){
+    console.log('hand 1 wins');
+    players[0] = players[0].concat(pot); //concat - merges 2 or more arrays. Does not change the existing arrays, returns a new array
+  } else if(card1.cardValue < card2.cardValue){
+    console.log('hand 2 wins');
+    players[0] = players[0].concat(pot);
+  } else {
+    console.log('tie'); //enter the war
+  }
+    console.log(players);
+  }
+ 
+
+
+function showCard(c,p){
+  let move = p * 40;
+  let bgColor = (c.icon == 'H' || c.icon == 'D') ? 'red' : 'black';
+  let bCard = c.num + ' & ' + c.suit + ';';
+  console.log(c,move);
+  return bCard;
+}
+
+function buildCards(){
+  deck = [];
+  for(s in suits){
+    let suit = suits[s][0].toUpperCase(); //returns value within const suits, 
+    //loop through suits
+    for(n in cardFace){
+     let card = {
+      suit: suits[s],
+      num: cardFace[n],
+      cardValue: parseInt(n) +2, //parseInt parses a string argument and returns an integer
+      icon:suit
+
+     } 
+     deck.push(card);
     }
+    //console.log(deck);
   }
-
-//const deck1= new Deck();
-//console.log(deck1.deck);
-
-//shuffle the deck
-shuffle(){
-  const deck = this.deck;
-  let m = deck.length, i;
-
-  while(m) {
-    i = Math.floor(Math.random() * m--);
-
-    [deck[m], deck[i]= [deck[i], deck [m]]];
-  }
-  return this;
 }
-  deal(){
-  return this.deck.pop();
+
+//dealing cards
+function dealCards(array){
+  for(let i=0; i<array.length; i++){
+   let m = i % 2;
+   players[m].push(array[i]);
+  }  
+  //console.log(players);
 }
+
+function goToWar(pot){
+  let card1, card2;
+  let position = (pot.length/2);
+  if((players[0].length <4) || (players[1].length <4)){
+    return;
+  } else {
+    for(let i = 0; i < 4; i++){
+      card1 = players[0].shift();
+      pot = pot.concat(card1);
+      p1.innerHTML += showCard(card1, (position + i));
+    }
+    for(let i = 0; i < 4; i++){
+      card2 = players[1].shift();
+      pot = pot.concat(card2);
+      p2.innerHTML += showCard(card2, (position + i));
   }
+  checkWinner(card1,card2,pot);
+  }
+}
 
-  const deck1 = new Deck();
-  deck1.shuffle()
-  console.log(deck1.deck);
-  deck1.deal()
-  console.log(deck1.deck);
-
-//dealing the cards
-
-
+//shuffle the deck 
+function shuffleArray(array){
+  for(let x = array.length -1;x>0;x--){
+    let ii = Math.floor(Math.random() * (x+1));
+    let temp = array[x];
+    array[x] = array[ii];
+    array [ii] = temp;
+  }
+  return array;
+}
